537008b4e6319c3605fabd1a319a43bf6b27fed9
Len Chang <lamgo.chang@gmail.com>
2020-03-24 15:27:54 +0800

    31 to 41 - Props System - Showing Custom Children

--- src/index.js
+++ src/index.js
@@ -2,29 +2,43 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 import faker from "faker";
 
-const getTime = () => {
-  return new Date().toLocaleTimeString();
+const getTime = (date, minutes) => {
+  return new Date(date.getTime() + minutes * 60000).toLocaleTimeString();
 };
+
+const now = new Date();
+const num = Math.floor(Math.random() * 100);
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">{getTime()}</span>
-          </div>
-          <div className="text">Nice blog post!</div>
+      <ApprovalCard>
+        <div>
+          <h4>Warning</h4>
+          Are you sure you want to do this ?
         </div>
-      </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Len"
+          timeAgo={getTime(now, num)}
+          content="Nice blog post"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Ralph"
+          timeAgo={getTime(now, num)}
+          content="I like the subject"
+        />
+      </ApprovalCard>
     </div>
   );
 };
